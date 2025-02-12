89b62e3c55dee745546dbd75e7082be874d86eaa
testgithub292 <139734979+testgithub292@users.noreply.github.com>
2025-02-12 22:15:25 +0500

    add  big video ron

--- tabs 1/tabs1.js
+++ tabs 1/tabs1.js
@@ -45,4 +45,16 @@
   /*----------------------------------------------------------------------*/
   
   
-  
+    // Get the navbar
+    const navbar = document.querySelector('.navbar');
+
+    // Listen for scroll event
+    window.addEventListener('scroll', function() {
+      if (window.scrollY >= 100) {
+        navbar.classList.add('scrolled');
+        navbar.classList.add('hidden');
+      } else {
+        navbar.classList.remove('scrolled');
+        navbar.classList.remove('hidden');
+      }
+    });
--- tangi token/scripttangitoken.js
+++ tangi token/scripttangitoken.js
@@ -44,6 +44,21 @@ scrollTopBtn.onclick = function() {
 
 
 
-  
+  //-----------------------------------------------------
+
+
+    // Get the navbar
+    const navbar = document.querySelector('.navbar');
+
+    // Listen for scroll event
+    window.addEventListener('scroll', function() {
+      if (window.scrollY >= 100) {
+        navbar.classList.add('scrolled');
+        navbar.classList.add('hidden');
+      } else {
+        navbar.classList.remove('scrolled');
+        navbar.classList.remove('hidden');
+      }
+    });
 
 
