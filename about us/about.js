cd9e2253fa241c7feb64ebc3b076cc1aad2bd207
testgithub292 <139734979+testgithub292@users.noreply.github.com>
2025-02-12 19:29:56 +0500

    update nav bar in 20 page

--- about us/about.js
+++ about us/about.js
@@ -18,3 +18,18 @@
       document.documentElement.scrollTop = 0; // Pro Chrome, Firefox, IE a Opera
   }
 /*----------------------------------------------------------------------*/
+
+
+  // Get the navbar
+  const navbar = document.querySelector('.navbar');
+
+  // Listen for scroll event
+  window.addEventListener('scroll', function() {
+    if (window.scrollY >= 100) {
+      navbar.classList.add('scrolled');
+      navbar.classList.add('hidden');
+    } else {
+      navbar.classList.remove('scrolled');
+      navbar.classList.remove('hidden');
+    }
+  });
--- project_info/project info.js
+++ project_info/project info.js
@@ -1,3 +1,16 @@
+    // Get the navbar
+    const navbar = document.querySelector('.navbar');
+
+    // Listen for scroll event
+    window.addEventListener('scroll', function() {
+      if (window.scrollY >= 100) {
+        navbar.classList.add('scrolled');
+        navbar.classList.add('hidden');
+      } else {
+        navbar.classList.remove('scrolled');
+        navbar.classList.remove('hidden');
+      }
+    });
   
   
   /*-------road map script ---------------------------*/
