64729b8250175042fe3b7ec0176da9a4bbe5a843
testgithub292 <139734979+testgithub292@users.noreply.github.com>
2025-02-13 09:59:48 +0500

    add  page tangi token

--- token information/token_information.js
+++ token information/token_information.js
@@ -50,3 +50,20 @@
   window.onload = function() {
     document.getElementById('infoParagraph').classList.add('show');
 };
+
+
+/*-----------------------------------------*/
+
+    // Get the navbar
+    const navbar = document.querySelector('.navbar');
+
+    // Listen for scroll event
+    window.addEventListener('scroll', function() {
+      if (window.scrollY >= 100) {
+        navbar.classList.add('scrolled');
+        navbar.classList.add('hidden');
+      } else {
+        navbar.classList.remove('scrolled');
+        navbar.classList.remove('hidden');
+      }
+    });
